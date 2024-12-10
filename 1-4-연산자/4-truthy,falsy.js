9bd7ee9d40ee8e4f9e9e915f012845f99e149b42
burgerkingzzang <jonathankim098@daum.net>
2024-12-10 14:34:26 +0900

    4-truthy,falsy

--- 1-4-연산자/4-truthy,falsy.js
+++ 1-4-연산자/4-truthy,falsy.js
@@ -1,5 +1,6 @@
 // truthuy, falsy : js는 논리가 아닌 데이터를 논리로 표현할 수 있다.
 // 0, '' , null, undefined , NaN ->> false로 취급한다. 나머지는 전부 true
+
 if (false) console.log('참입니다1');  //false
 if (0) console.log('참입니다2');  //false
 if (null) console.log('참입니다3');  //false
