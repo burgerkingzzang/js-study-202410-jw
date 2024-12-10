0b66524821a4f5738af5e5861591b5c8e09b3d19
burgerkingzzang <jonathankim098@daum.net>
2024-12-10 17:39:37 +0900

    elseif

--- 1-5-조건문/2-else-if.js
+++ 1-5-조건문/2-else-if.js
@@ -12,3 +12,22 @@ else if(age >= 8){
 else{
   console.log('미취학 아동입니다.');}
 //elseif 와 if의 차이는 그룹화의 차이 
+
+
+
+
+
+
+let age1 =17;
+
+if (age1 > 19 ) {
+  console.log('성인입니다.');
+} else if (age1 >= 17) {
+  console.log('고등학생입니다.');
+} else if (age1 >= 14) {
+  console.log('중학생입니다.');
+} else if (age1 >=8) {
+  console.log('초등학생입니다.');
+} else {
+  console.log('미취학 아도입니다.')
+}
